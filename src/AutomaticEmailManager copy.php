9256207a000eb9843b39a8289824f7b13e7ccffd
David <daivdpro@tuta.io>
2023-07-03 16:57:55 +0200

    added function to build template

--- src/AutomaticEmailManager copy.php
+++ src/AutomaticEmailManager copy.php
@@ -49,6 +49,7 @@ class AutomaticEmailManager
     {
     $this->languages = Language::getInstance()->all();
     }
+
     private function getLanguage()
     {
     $fallbackLanguage = Language::getInstance()->filterCode($this->languages, 'en');
@@ -62,6 +63,75 @@ class AutomaticEmailManager
         }
     
     }
+
+    private function getTemplate($language)
+    {
+        return array_filter(
+            $this->automaticEmail->templates,
+            fn($template) => $template->language->code === $language->code
+    )[0];
+    }
+        
+    private function isTemplateEnabled($template)
+    {
+        return (!isset($template->isEnabled) || !$template->isEnabled) && !$this->isForPreview;
+    }
+
+    private function replaceToday($template)
+    {
+    $this->replaceVar('today', (new DateTime())->format('d/m/Y'), $template->content);
+    }
+
+    private function replaceIntroduction($template)
+    {
+        if (isset($this->params['introduction'])) {
+
+    $this->replaceVar('introduction', str_replace("\n", '<br>', $this->params['introduction']), $template->content);
+    }
+    }
+
+    private function replaceRecipientVars($template)
+    {
+        if (isset($this->data['recipient'])) {
+    $prefix = 'recipient';
+    
+            $this->replaceVar($prefix . '_firstname', $this->data['recipient']->firstName, $template->content);
+            $this->replaceVar($prefix . '_name', $this->data['recipient']->lastName, $template->content);
+            $this->replaceVar($prefix . '_email', $this->data['recipient']->email, $template->content);
+            $this->replaceVar($prefix . '_phone', $this->data['recipient']->phone, $template->content);
+        }
+    
+    }
+
+    private function replaceProductVars($template)
+    {
+        if (isset($this->data['product'])) {
+    $prefix = 'product';
+    
+            $this->replaceVar($prefix . '_title', $this->data['product']->title, $template->content);
+            $this->replaceVar($prefix . '_city', $this->data['product']->city, $template->content);
+            $this->replaceVar($prefix . '_model', $this->data['product']->model, $template->content);
+            $this->replaceVar($prefix . '_builder', $this->data['product']->builder, $template->content);
+        }
+    
+    }
+    private function replaceSignature($template)
+    {
+        if (isset($this->params['signature'])) {
+    if ($this->params['signature'] === true) {
+                if (isset($data['sender']) && null !== $language) {
+                    $this->params['signature'] = Translator::getInstance()->translate($language, 'DefaultAutomaticEmailSignature');
+    } else {
+    $this->params['signature'] = '';
+    }
+    }
+    
+            $this->replaceVar('signature', str_replace("\n", '<br>', $this->params['signature']), $template->signature);
+        }
+    
+    }
+
+
 
 
 
